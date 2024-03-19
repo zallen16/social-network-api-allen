b49d491d534374bf111518fd78eea443bfa25ed4
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-03-18 22:18:32 -0400

    completed user controller

---
+++ controllers/userControllers.js
@@ -0,0 +1,70 @@
+const { User, Thought } = require('./models');
+
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find().populate('thoughts');
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+            .populate('thoughts');
+
+            if (!user) {
+                return res.status(404).json({ message: 'No users with that ID' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.courseId });
+
+            if (!user) {
+                res.status(4040).json({ message: 'No user with that ID' });
+            } 
+
+            await Thought.deleteMany({ _id: { $in: user.thoughts } });
+            res.json({ message: 'User and thoughts deleted.' });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                res.status(4040).json({ message: 'No user with this Id!' });
+            }
+
+            res.json(user);
+        } catch (erro) {
+            res.status(500).json(err);
+        }
+    },
+};
